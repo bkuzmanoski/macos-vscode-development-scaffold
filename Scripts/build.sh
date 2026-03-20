6f76c6ef60a420e85a249b4f65932c964ab554d4
Brian Kuzmanoski <bkuzmanoski@icloud.com>
2026-03-21 10:30:12 +1100

    Update build script to reliably override Xcode derived data location settings

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -14,8 +14,7 @@ readonly DESTINATION="platform=macOS,arch=arm64"
 # Paths
 readonly SCRIPT_DIR="${0:A:h}"
 readonly PROJECT_DIR="${SCRIPT_DIR}/.."
-readonly DERIVED_DATA_DIR="${PROJECT_DIR}/DerivedData"
-readonly BUILD_LOG_PATH="${DERIVED_DATA_DIR}/xcodebuild.log"
+readonly BUILD_LOG_PATH="${PROJECT_DIR}/DerivedData/xcodebuild.log"
 
 # Dependencies
 readonly -a REQUIRED_COMMANDS=("xcodebuild" "xcbeautify" "xcode-build-server")
@@ -51,6 +50,10 @@ xcodebuild build \
   -scheme "${BUILD_SCHEME}" \
   -configuration "${BUILD_CONFIGURATION}" \
   -destination "${DESTINATION}" \
-  -derivedDataPath "${DERIVED_DATA_DIR}" \
+  -IDEBuildLocationStyle="Custom" \
+  -IDECustomDerivedDataLocation="DerivedData" \
+  -IDECustomBuildLocationType="RelativeToDerivedData" \
+  -IDECustomBuildIntermediatesPath="Build/Intermediates.noindex" \
+  -IDECustomBuildProductsPath="Build/Products" \
   | tee "${BUILD_LOG_PATH}" \
   | xcbeautify --disable-logging
